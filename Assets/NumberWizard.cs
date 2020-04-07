e31d7e89dc1a4cf405814d29341753290c7ed528
IdaMariaMelen <ida.maria.melen@gmail.com>
2020-04-07 12:07:14 +0200

    Practicing

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizar, yo"); //print(); er like bra
+        Debug.Log("Hughest Number you can pick is 100");
+        Debug.Log("Lowest Number is 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Update Arrow key was pressed.");
+        }   
+    }
+}
